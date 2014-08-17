ed73f1f5ca0637ab550df2ff0adf90864192eeb6
Joel Erickson <Erickson@JME-Mac-Pro.local>
2014-08-16 21:25:40 -0700

    added a test class

---
+++ Git Test/GTTest.h
@@ -0,0 +1,13 @@
+//
+//  GTTest.h
+//  Git Test
+//
+//  Created by Joel Erickson on 8/16/14.
+//  Copyright (c) 2014 Gnarly Tooth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTest : NSObject
+
+@end
---
+++ Git Test/GTTest.m
@@ -0,0 +1,13 @@
+//
+//  GTTest.m
+//  Git Test
+//
+//  Created by Joel Erickson on 8/16/14.
+//  Copyright (c) 2014 Gnarly Tooth. All rights reserved.
+//
+
+#import "GTTest.h"
+
+@implementation GTTest
+
+@end
